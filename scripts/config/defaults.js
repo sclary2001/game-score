76e1450f928f196ec0ce08adfd39c2e90a969c9c
sclary2001 <seanclary.2001@gmail.com>
2026-04-01 12:59:43 -0500

    Update default leaderboard player name

--- scripts/config/defaults.js
+++ scripts/config/defaults.js
@@ -1,7 +1,7 @@
 export const DEFAULT_PLAYER_NAME = 'Sean Clary';
 
 export const DEFAULT_LEADERBOARD = [
-  { name: 'Garfield', score: 9000 },
+  { name: 'Sean Clary', score: 9000 },
   { name: 'Nermal', score: 7000 },
   { name: 'Odie', score: 5000 },
   { name: 'Arlene', score: 3000 }
